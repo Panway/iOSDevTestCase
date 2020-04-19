7597b79955c523340abebc32c32d4720c0a5210d
WeirdPan <wooodypan@foxmail.com>
2020-04-19 20:14:56 +0800

    init Xcode Proj

---
+++ iOSDevTips/iOSDevTips/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSDevTips
+//
+//  Created by panwei on 2020/4/19.
+//  Copyright © 2020 WeirdPan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ iOSDevTips/iOSDevTips/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSDevTips
+//
+//  Created by panwei on 2020/4/19.
+//  Copyright © 2020 WeirdPan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
